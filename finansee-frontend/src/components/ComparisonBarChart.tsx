c3faaca046926f1583ce72b6efb110628c2ffd91
KarmotrineOverflow <elfiemarvinjohncampit@gmail.com>
2025-11-14 16:01:30 +0800

    Started adding comparison bar chart functionality

--- finansee-frontend/src/components/ComparisonBarChart.tsx
+++ finansee-frontend/src/components/ComparisonBarChart.tsx
@@ -1,10 +1,42 @@
-export default function ComparisonBarChart() {
+import Chart from 'chart.js/auto'
+type ReportProp = {
+    reportProp: Array<Object>
+}
+
+type ReportData = {
+    month: string,
+    totalMoney: Float32Array
+}
+
+export default function ComparisonBarChart({ reportProp }: ReportProp) {
 
     // Need a chart lib here to give visualization to the received data
     // A bar chart will display up to the last 4 months (if data is present) of total money to show difference
+    /* 
+        ReportData prop entries contains
+        - Month
+        - Total Money
+    */
+
+    // FIXME: Make the mapping work
+    const rowData = reportProp.map(row => row.month)
+
+    new Chart(
+        document.getElementById('previous-month-comparison'),
+        {
+            type: "bar",
+            data: {
+                reportData
+            }
+        }
+    )
+
 
     return (
 
-
+        <div>
+            <p>Previous Month Comparison</p>
+            <canvas id="previous-month-comparison" />            
+        </div>
     )
 }
---
+++ finansee-frontend/src/components/QuoteOfTheDay.tsx
@@ -0,0 +1,13 @@
+
+
+export default function QuoteOfTheDay() {
+
+    // Check if quote of the day exists in backend server
+    // If not, retrieve one from API and save to backend server path
+    // Implement in backend to use 'fs'
+
+    return (
+
+
+    )
+}
